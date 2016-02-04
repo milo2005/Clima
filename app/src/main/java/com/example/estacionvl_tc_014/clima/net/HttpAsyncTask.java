b977ace84e13646d8b6de96b72d3e64f30ed56c3
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-03 20:04:51 -0500

    Clase HttpConnection Implementada

---
+++ app/src/main/java/com/example/estacionvl_tc_014/clima/net/HttpAsyncTask.java
@@ -0,0 +1,18 @@
+package com.example.estacionvl_tc_014.clima.net;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by EstacionVL-TC-014 on 3/02/16.
+ */
+public class HttpAsyncTask extends AsyncTask<String, Integer, String> {
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+    }
+}
